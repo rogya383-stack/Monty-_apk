dbb00c94b34cb8cd78c0a095df484aa05c35e3de
rogya383-stack <rogya383@gmail.com>
2025-12-06 13:36:51 +0200

    Create utils package for PDF Reader
    Added utilities package for Professional PDF Reader with theme management.

---
+++ core/core/gui/gui/gui/ gui/gui/utils/__init__.py
@@ -0,0 +1,14 @@
+"""
+Utilities package for Professional PDF Reader
+Contains helper functions, theme management, and utilities
+"""
+
+__version__ = "1.0.0"
+__author__ = "PDF Reader Team"
+
+# استيرادات رئيسية
+from .themes import ThemeManager
+
+__all__ = [
+    'ThemeManager'
+]
